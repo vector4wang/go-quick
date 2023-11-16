cd21fdbfaff1460f8bebcd4fd5e394f24c7d57ea
wangxc <772704457@qq.com>
2023-11-16 23:17:26 +0800

    gorm

---
+++ 01-框架/orm/core/config.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"gopkg.in/yaml.v3"
+	"os"
+)
+
+var cf *Config
+
+func main() {
+	fmt.Println(cf.Url)
+}
+
+func init() {
+	var c     Config
+	cf = c.
+}
+
+// profile variables
+type Config struct {
+	Username string `yaml:"username"`
+	Password string `yaml:"password"`
+	Url      string `yaml:"url"`
+	Database string `yaml:"database"`
+	Test     string `yaml:"test"`
+}
+
+func (c *Config) getConf() *Config {
+	yamlFile, err := os.ReadFile("conf.yaml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	err = yaml.Unmarshal(yamlFile, c)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	return c
+}
---
+++ 01-框架/orm/core/gorm_demo.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
+	"os"
+	"time"
+)
+
+/**
+https://www.cnblogs.com/kylin5201314/p/15484473.html
+*/
+
+// 定义数据模型
+type UserInfo struct {
+	ID         uint      `gorm:"primary_key" json:"id"`
+	Name       string    `gorm:"size:255" json:"name"`
+	Age        uint      `json:age`
+	Birthday   time.Time `json:birthday`
+	CreateTime time.Time `gorm:"-" json:createTime`
+	UpdateTime time.Time `gorm:"-" json:updateTime`
+}
+
+func main() {
+
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold: time.Second, // 慢 SQL 阈值
+			LogLevel:      logger.Info, // Log level
+			Colorful:      true,        // 禁用彩色打印
+		},
+	)
+
+	db, err := gorm.Open(mysql.New(mysql.Config{
+		DSN: "root:123456@tcp(127.0.0.1:3306)/gorm?charset=utf8mb4&parseTime=True&loc=Local", // 账号密码地址端口
+	}), &gorm.Config{
+		Logger: newLogger,
+	})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// 直接通过上面连接得到的db，调用DB()函数返回即可设置
+	sqlDB, _ := db.DB()
+
+	// SetMaxIdleConns 设置空闲连接池中连接的最大数量
+	sqlDB.SetMaxIdleConns(10)
+
+	// SetMaxOpenConns 设置打开数据库连接的最大数量。
+	sqlDB.SetMaxOpenConns(100)
+
+	// SetConnMaxLifetime 设置了连接可复用的最大时间。
+	sqlDB.SetConnMaxLifetime(time.Hour)
+
+	fmt.Println(db)
+
+	// 自动迁移数据库结构
+	db.AutoMigrate(&UserInfo{})
+
+	// 创建一条数据
+	bd, err := time.Parse(time.DateTime, "2023-11-16 19:00:35")
+	fmt.Println(bd)
+	db.Create(&UserInfo{Name: "面包", Age: 3, Birthday: bd})
+
+	// 查询所有数据
+	var userInfos []UserInfo
+	db.Find(&userInfos)
+	fmt.Println("所有用户：", userInfos)
+	// 更新一条数据
+	db.Model(&UserInfo{}).Where("name = ?", "面包").Update("age", 4)
+
+	// 删除一条数据
+	db.Where("name = ?", "面包1").Delete(&UserInfo{})
+
+}
