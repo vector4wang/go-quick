00413325541677b646feac843eceaac1cf3a1e13
wangxc <772704457@qq.com>
2024-01-31 19:07:45 +0800

    refresh

--- 02-GoZero/go-zero-sse/core/internal/config/config.go
+++ 02-GoZero/go-zero-sse/core/internal/config/config.go
@@ -1,7 +1,11 @@
 package config
 
-import "github.com/zeromicro/go-zero/rest"
+import (
+	"github.com/zeromicro/go-zero/core/logx"
+	"github.com/zeromicro/go-zero/rest"
+)
 
 type Config struct {
 	rest.RestConf
+	LogConf logx.LogConf
 }
